092e6dc6d3a25378d5216c40a5ff8670d8c352b7
DarkeGhost <55811898+DarkeGhost@users.noreply.github.com>
2020-03-17 15:47:35 -0400

    Added Script
    Funtionality added to the level selection, script added for selecting the level

--- Assets/Scripts/CreditButtons.cs
+++ Assets/Scripts/CreditButtons.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class CreditButtons : MonoBehaviour {
 
     // Upon pressing Return to Main Menu, the first scene is loaded (main menu)
