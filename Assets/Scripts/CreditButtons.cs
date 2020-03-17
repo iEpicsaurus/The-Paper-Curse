8af98301ea4c964166f9587a971044138c5ff908
DarkeGhost <55811898+DarkeGhost@users.noreply.github.com>
2020-03-17 15:37:44 -0400

    Added Script
    Quit and main menu buttons work on the credit scene

---
+++ Assets/Scripts/CreditButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreditButtons : MonoBehaviour {
+
+    // Upon pressing Return to Main Menu, the first scene is loaded (main menu)
+	public void ReturnToMain() {
+
+		SceneManager.LoadScene(0);
+
+	}
+
+	// Upon pressing Quit, the application will terminate.
+	public void QuitGame() {
+
+		Application.Quit();
+
+	}
+}
