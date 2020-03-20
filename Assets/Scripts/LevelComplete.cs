d5a605d9fc27fde18af5f8eaffcb7a4b5d4dd5ab
DarkeGhost <55811898+DarkeGhost@users.noreply.github.com>
2020-03-20 18:30:11 -0400

    Revert "Added Level complete SFX and added SFX to beginner and medium levels"
    This reverts commit 067de040dbf8689fa2b177e9f694fe29aeb0dff2.

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -6,14 +6,11 @@ using UnityEngine.SceneManagement;
 // This class deals with the scenario when the level has been completed (flower has been reached)
 public class LevelComplete : MonoBehaviour {
 
-	public AudioSource Level_Complete;
-
 	// Upon a collision being triggered with the player, move to the next scene (level complete scene)
     private void OnTriggerEnter2D(Collider2D collision) {
 
     	if(collision.gameObject.CompareTag("Player")) {
 
-			Level_Complete.Play();
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
 		}
