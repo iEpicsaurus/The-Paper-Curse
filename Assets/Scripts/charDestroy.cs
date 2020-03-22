07f147c86ba6942efa8275a170dbcde4d1daf679
ksnmryy <44381292+ksnmryy@users.noreply.github.com>
2020-03-22 12:01:15 -0400

    Added Coin Indicator
    Completes sub-tasks from trello task UI/UX: Add Coin Indicator

--- Assets/Scripts/charDestroy.cs
+++ Assets/Scripts/charDestroy.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 // This class deals with character behaviour on collision with other objects
 public class charDestroy : MonoBehaviour {
 
 	public AudioSource PaperTear;
 	public AudioSource CoinSound;
+
+	public Text coinsUI;
+	private int coinsCollected;
+
+	void Start ()
+    {
+		// Setting up coin UI
+		coinsCollected = 0;
+		coinsUI = GameObject.Find("CoinsUI").GetComponent<Text>();
+		coinsUI.text = "Coins: " + coinsCollected;
+	}
 
 	public async void OnCollisionEnter2D(Collision2D collision) {
 
@@ -26,7 +38,11 @@ public class charDestroy : MonoBehaviour {
 			CoinSound.Play();
 			Destroy(collision.gameObject);
 
-		}		
+			// Update Coin indicator UI
+			coinsCollected++;
+			coinsUI.text = "Coins: " + coinsCollected;
+
+		}
 
 	}
 
